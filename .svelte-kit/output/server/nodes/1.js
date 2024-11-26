425a183400d165be928309d4a631f44404aa66c5
retrospatial <florinasutanto@gmail.com>
2024-11-26 11:46:22 -0500

    rewrite to subpath

--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.B9u-Sl01.js","_app/immutable/chunks/scheduler.CjypH3xl.js","_app/immutable/chunks/index.uIIOWwRv.js","_app/immutable/chunks/entry.BhnJelQi.js","_app/immutable/chunks/index.Duek6dXD.js","_app/immutable/chunks/paths.DCD-n7W_.js"];
+export const imports = ["_app/immutable/nodes/1.DP2QetBW.js","_app/immutable/chunks/scheduler.CjypH3xl.js","_app/immutable/chunks/index.uIIOWwRv.js","_app/immutable/chunks/entry.CvXm0icO.js","_app/immutable/chunks/index.Duek6dXD.js","_app/immutable/chunks/paths.BKZzovpz.js"];
 export const stylesheets = [];
 export const fonts = [];
---
+++ .vercel/output/static/_app/immutable/entry/start.C6YnHgt2.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CvXm0icO.js";export{t as start};
