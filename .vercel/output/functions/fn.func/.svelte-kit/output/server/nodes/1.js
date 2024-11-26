a4dce0da9c8dc29992724a299b98412fecaaaeeb
retrospatial <florinasutanto@gmail.com>
2024-11-26 11:45:16 -0500

    rewrite to subpath

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.ChXYZoOt.js","_app/immutable/chunks/scheduler.CjypH3xl.js","_app/immutable/chunks/index.uIIOWwRv.js","_app/immutable/chunks/entry.VgGPy25o.js","_app/immutable/chunks/index.Duek6dXD.js","_app/immutable/chunks/paths.DBkW0_CJ.js"];
+export const imports = ["_app/immutable/nodes/1.B9u-Sl01.js","_app/immutable/chunks/scheduler.CjypH3xl.js","_app/immutable/chunks/index.uIIOWwRv.js","_app/immutable/chunks/entry.BhnJelQi.js","_app/immutable/chunks/index.Duek6dXD.js","_app/immutable/chunks/paths.DCD-n7W_.js"];
 export const stylesheets = [];
 export const fonts = [];
--- .vercel/output/static/_app/immutable/entry/start.DhNpyE7G.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.VgGPy25o.js";export{t as start};
